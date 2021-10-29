7d132a9305059d784f5f702d81fa5bfbbd6b15dc
gitHuang <1369936413@qq.com>
2021-10-29 17:47:57 +0800

    branch1

---
+++ src/main/java/com/huang/git/GitTest2.java
@@ -0,0 +1,11 @@
+package com.huang.git;
+
+/**
+ * @author: Mr.Huang
+ * @create: 2021-10 29-17:32
+ **/
+public class GitTest2 {
+    public static void main(String[] args) {
+        System.out.println("i am a branch");
+    }
+}
